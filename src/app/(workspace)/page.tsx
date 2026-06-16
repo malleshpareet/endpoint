7fbb554644a56f420f58232265719f915d80c749
Mallesh Pareet <malleshpareet360@gmail.com>
2026-06-16 16:28:55 +0530

    60_collections

--- src/app/(workspace)/page.tsx
+++ src/app/(workspace)/page.tsx
@@ -1,14 +1,48 @@
-import { Button } from "@/components/ui/button";
-import { currentUser } from "@/modules/authentication/actions";
-import UserButton from "@/modules/authentication/components/user-button";
-import Image from "next/image";
+"use client";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import TabbedSidebar from "@/modules/collections/components/sidebar";
+import { useWorkspaceStore } from "@/modules/layout/stores";
+
+// import RequestPlayground from "@/modules/request/components/request-playground";
 
 
-export default async function Home() {
-  const user = await currentUser()
+
+import { useGetWorkspace } from "@/modules/workspace/hooks/workspace";
+import { Loader } from "lucide-react";
+
+const Page = () => {
+  const { selectedWorkspace } = useWorkspaceStore();
+  const { data: currentWorkspace, isLoading } = useGetWorkspace(selectedWorkspace?.id!);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-full">
+        <Loader className="animate-spin h-6 w-6 text-indigo-500" />
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <UserButton user={user}/>
-    </div>
-  );
-}
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel defaultSize={65} minSize={40}>
+        {/* <RequestPlayground /> */}
+        <h1> RequestPlayground</h1>
+      </ResizablePanel>
+
+      <ResizableHandle withHandle />
+
+      <ResizablePanel defaultSize={35} maxSize={40} minSize={25} className="flex">
+        <div className="flex-1">
+          <TabbedSidebar currentWorkspace={currentWorkspace} />
+        </div>
+      </ResizablePanel>
+    </ResizablePanelGroup>
+  )
+};
+
+export default Page;
